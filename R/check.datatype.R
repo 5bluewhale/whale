82d445f7c58f299f63a96ec14f69da6354341333
5bluewhale <65381148+5bluewhale@users.noreply.github.com>
2020-06-18 16:29:16 +0900

    check.datatype

---
+++ R/check.datatype.R
@@ -0,0 +1,45 @@
+#'
+#'
+#' If it is an integer, change it to numeric and check the character and numeric variables
+#' After roading the data, you must name the data as 'data'!!!!
+#'
+#' @param data input data
+#'
+#' @return You can check the type for each variable.
+#'
+#' @examples
+#' check.datatype()
+#' enter : data
+#' enter : colnumber
+#'
+#' @export
+check.datatype <- function(){
+  print("Please enter a data name")
+  dataname<-scan( what = 'character',n=1)
+  assign(dataname,data)
+  dataset<-get(dataname)
+  colname<-colnames(data)
+  for(j in 1:ncol(dataset)){
+    if(is.integer(dataset[,colname[j]])){
+      dataset[,colname[j]]<-as.numeric(dataset[,colname[j]])
+    }
+  }
+  print("Enter the number of variables")
+  colnumber<-scan()
+  for(i in colnumber){
+    if(typeof(dataset[,i])=="double"){
+      type <- "numeric"
+      valuename<-colnames(dataset[i])
+    }else if(is.factor(dataset[,i])){
+      type <- "factor"
+      valuename<-colnames(dataset[i])
+    }else{
+      type <- "charactor"
+      valuename<-colnames(dataset[i])
+    }
+    cat("The" ,valuename ," variable type is",type,"\n")
+  }
+}
+
+
+
